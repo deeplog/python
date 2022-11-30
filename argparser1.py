62858104c6067c1f4ce18218c6f8f07a9906a66a
kwanwoo kim <kwanwoo.kim@gmail.com>
2022-12-01 04:48:14 +0900

    argparser 리팩토링

--- argparser1.py
+++ argparser1.py
@@ -2,26 +2,33 @@
 
 import argparse
 
-example_text = '''example:
- python argparser1.py --epoch 200 --batch_size 128 --name "My Test" 
- '''
+def print_test(args):
+    print(args.epoch)
+    print(args.batch_size)
+    print(args.lr_initial)
+    print(args.name)
 
-# 인자값을 받을 수 있는 인스턴스 생성
-parser = argparse.ArgumentParser(description='Argparse Tutorial',
-                                 epilog = example_text,
-                                 formatter_class=argparse.RawDescriptionHelpFormatter)
+def argparser():
+    example_text = '''example:
+      python argparser1.py --epoch 200 --batch_size 128 --name "My Test" 
+      '''
 
-# 입력받을 인자값 설정 (default 값 설정가능)
-parser.add_argument('--epoch',          type=int,   default=150)
-parser.add_argument('--batch_size',     type=int,   default=128)
-parser.add_argument('--lr_initial',     type=float, default=0.1)
-parser.add_argument('--name',     type=str, default="None")
+    # 인자값을 받을 수 있는 인스턴스 생성
+    parser = argparse.ArgumentParser(description='Argparse Tutorial',
+                                     epilog=example_text,
+                                     formatter_class=argparse.RawDescriptionHelpFormatter)
 
-# args 에 위의 내용 저장
-args    = parser.parse_args()
+    # 입력받을 인자값 설정 (default 값 설정가능)
+    parser.add_argument('--epoch', type=int, default=150)
+    parser.add_argument('--batch_size', type=int, default=128)
+    parser.add_argument('--lr_initial', type=float, default=0.1)
+    parser.add_argument('--name', type=str, default="None")
 
-# 입력받은 인자값 출력
-print(args.epoch)
-print(args.batch_size)
-print(args.lr_initial)
-print(args.name)
+    # args 에 위의 내용 저장
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    args = argparser()
+    print_test(args)
